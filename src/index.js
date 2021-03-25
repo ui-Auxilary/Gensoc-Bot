d17b9786e4caf1c2599d33d277f8ef9e58f374f3
ui-Auxilary <430285700@student.sbhs.nsw.edu.au>
2021-03-25 11:23:50 +1100

    Port

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-# port (as described above) and host are both wrong
 const host = 'localhost';
 const port = 3000;
 
-# use alternate localhost and the port Heroku assigns to $PORT
 const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
