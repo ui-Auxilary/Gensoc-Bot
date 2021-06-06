ecddf4207f70d17d47e0fb08e5dbd9ede3c4e7cc
ui-Auxilary <430285700@student.sbhs.nsw.edu.au>
2021-06-07 00:19:26 +1000

    Fix

--- src/index.js
+++ src/index.js
@@ -20,14 +20,12 @@ client.on('ready', () => {
 
 client.on('guildMemberAdd', (member) => {
     console.log('User ' + member.user.username + ' has joined the server!');
-    defaultRole = member.guild.roles.cache.find(role => role.name === "Traveller");
-    member.roles.add(defaultRole);
 });
 
 client.on('message', (message) => {
   if (message.channel.id == 822423063697948693) {
     console.log(message.guild.id);
-    verified_role = message.guild.roles.cache.find(role => role.name === "verified");
+    verified_role = message.guild.roles.cache.find(role => role.name === "Traveller");
     console.log(verified_role);
     let embed = message.embeds[0], field, text, number;
     if (!embed) return;
