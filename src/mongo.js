26e2667e360d55ea738dbbf51ab35bf5e3d46960
ui-Auxilary <66155270+ui-Auxilary@users.noreply.github.com>
2022-05-23 17:35:23 +1000

    Update mongo.js

--- src/mongo.js
+++ src/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoPath = "mongodb+srv://Gensoc:UHcFGTJ3MBVedH1C@gensocdb.pkjz1.mongodb.net/GensocDB?retryWrites=true&w=majority"
+const mongoPath = "HIDDEN"
 module.exports = async () => {
   await mongoose.connect(mongoPath, {
     keepAlive: true,
